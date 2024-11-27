680721831093f306adb398e81255addab99f8501
jeugregg <jeugregg@hotmail.com>
2024-11-27 18:06:46 +0100

    update strat balanced

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Contract, WalletAccount, uint256, constants, RpcProvider } from "starknet";
 import { connect } from '@starknet-io/get-starknet';
 import { BrianSDK } from "@brian-ai/sdk";
+
+const mode_debug = true;
+
+
 // Replace with actual token contract addresses
 const tokenAddresses_default = {
   ETH: "0x049D36570D4e46f48e99674bd3fcc84644DdD6b96F7C741B1562B82f9e004dC7",
@@ -10,6 +14,8 @@ const tokenAddresses_default = {
   USDT: "0x068F5c6a61780768455de69077E07e89787839bf8166dEcfBf92B645209c0fB8",
   WBTC: "0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac"
 };
+
+
 
 async function getMarket() {
   const response = await fetch('https://starknet.impulse.avnu.fi/v1/tokens', {
@@ -35,20 +41,19 @@ async function getMarket() {
 }
 
 function getMarketTokenSymbol() {
-  const storedTokens = localStorage.getItem('starknetTokens');
-  const tokens = JSON.parse(storedTokens);
+
   // get all token symbol 
   // tokens is a list of data like that : 
   // {"position":1,"name":"Ethereum","symbol":"ETH","address":"0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7","decim...
   //const tokenSymbols = tokens.map(token => token.symbol).filter(symbol => symbol != "\b8");
+  const tokens = loadTokens();
   const tokenSymbols = tokens.map(token => token.symbol);
   return tokenSymbols;
 }
 
 function getMarketTokenAddress() {
   // get all token address from localstorage
-  const storedTokens = localStorage.getItem('starknetTokens');
-  const tokens = JSON.parse(storedTokens);
+  const tokens = loadTokens();
   // get all token symbol 
   // tokens is a list of data like that : 
   // {"position":1,"name":"Ethereum","symbol":"ETH","address":"0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7","decim...
@@ -60,8 +65,7 @@ function getMarketTokenAddress() {
 
 function getMarketTokenPrice() {
   // get all token price from localstorage
-  const storedTokens = localStorage.getItem('starknetTokens');
-  const tokens = JSON.parse(storedTokens);
+  const tokens = loadTokens();
   // get all token symbol 
   // tokens is a list of data like that : 
   // {"position":1,"name":"Ethereum","symbol":"ETH","address":"0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7","decim...
@@ -70,6 +74,40 @@ function getMarketTokenPrice() {
   const tokenPrices = tokens.reduce((acc, token) => ({ ...acc, [token.symbol]: token.market.currentPrice }), {});
   return tokenPrices;
 }
+
+function extractPrices(data, tokenSymbol) {
+  if (!data || data.length === 0) {
+    return [];
+  }
+
+  // Find the token data based on the symbol
+  const tokenData = data.find(token => token.symbol === tokenSymbol);
+
+  if (!tokenData || !tokenData.linePriceFeedInUsd) {
+    return [];
+  }
+
+  return tokenData.linePriceFeedInUsd.map(priceData => priceData.value);
+}
+
+function loadTokens() {
+  const storedTokens = localStorage.getItem('starknetTokens');
+  const tokens = JSON.parse(storedTokens);
+  return tokens;
+}
+function getMarketTokenMcap() {
+  // get Market Cap from localstorage
+  const tokens = loadTokens();
+
+  const tokenMcaps = tokens.reduce((acc, token) => ({
+    ...acc,
+    [token.symbol]: token.market.marketCap === 0 ? token.market.starknetTvl : token.market.marketCap
+  }), {});
+
+  return tokenMcaps;
+}
+
+
 
 // Assuming brianBalances is an array of objects returned by the Brian API
 function extractBrianBalances(brianBalance) {
@@ -111,8 +149,38 @@ function extractAllBrianBalances(brianBalances) {
     }
   }
   return extractedBalances;
-
 }
+
+function calculateEMA7HourlyAndMaxStdDev(prices) {
+  let length = prices.length;
+  if (length < 168) {
+    if (length < 3) {
+      throw new Error("EMA7 need more longer prices history");
+    }
+  }
+
+  // Définir le multiplicateur
+  const multiplier = 2 / (length + 1);
+
+  // Calculer l'EMA en parcourant les prix du plus récent au plus ancien
+  let ema = prices[length - 1];
+  let lastWeekEMAs = [ema];
+
+  for (let i = length - 2; i >= 0; i--) {
+    ema = prices[i] * multiplier + ema * (1 - multiplier);
+    lastWeekEMAs.unshift(ema);
+    if (lastWeekEMAs.length > 168) {
+      lastWeekEMAs.pop();
+    }
+  }
+
+  // Calculer la variance maximale des 168 dernières MME
+  const meanEMA = lastWeekEMAs.reduce((sum, value) => sum + value, 0) / length;
+  const maxStdDev = Math.sqrt(Math.max(...lastWeekEMAs.map(value => Math.pow(value - meanEMA, 2)))) / meanEMA * 100;
+
+  return { ema, maxStdDev };
+}
+
 
 function App() {
   const [myWalletAccount, setMyWalletAccount] = useState(null);
@@ -123,6 +191,8 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [totalWalletValue, setTotalWalletValue] = useState(0);
   const [investmentAmount, setInvestmentAmount] = useState("");
+  const [selectedSolution, setSelectedSolution] = useState('Balanced'); // Set 'Balanced' as default
+  const [investmentBreakdown, setInvestmentBreakdown] = useState(null);
 
   // Access the API key from environment variables
   const brianApiKey = import.meta.env.VITE_BRIAN_API_KEY;
@@ -133,6 +203,78 @@ function App() {
   console.log("Brian API Key:", brianApiKey);
   const brian = new BrianSDK(options);
 
+  const handleSolutionSelect = (solution) => {
+    setSelectedSolution(solution);
+
+    // Replace with your logic to get investment breakdown for the selected solution
+    const breakdown = getInvestmentBreakdown(solution, investmentAmount);
+    setInvestmentBreakdown(breakdown);
+  };
+
+  // Placeholder function to simulate getting investment breakdown data
+  const getInvestmentBreakdown = (solution, amount) => {
+    // Replace this with your actual logic to fetch or calculate the breakdown
+    // based on the selected solution and investment amount.
+
+    if (solution === 'Secure') {
+      return {
+        USDC: 0.6 * amount,
+        ETH: 0.3 * amount,
+        WBTC: 0.1 * amount,
+      };
+    } else if (solution === 'Balanced') {
+      // select 10 first Market Cap
+      const tokenMcaps = getMarketTokenMcap();
+      let sortedTokens = Object.keys(tokenMcaps).sort((a, b) => tokenMcaps[b] - tokenMcaps[a]);
+      //sortedTokens = sortedTokens.slice(0, 10);
+      // calculate EMA7Hourly And MaxStdDev
+      const tokens = loadTokens();
+      const tokenPrices = getMarketTokenPrice();
+      let tokenEMA7Hourly = {};
+      let tokenMaxStdDev = {};
+      for (const token of sortedTokens) {
+        const tokenPriceHistory = extractPrices(tokens, token);
+        try {
+          const tokenEMA7HourlyAndMaxStdDev = calculateEMA7HourlyAndMaxStdDev(tokenPriceHistory);
+          tokenEMA7Hourly[token] = tokenEMA7HourlyAndMaxStdDev.ema;
+          tokenMaxStdDev[token] = tokenEMA7HourlyAndMaxStdDev.maxStdDev;
+
+          // detect stable coin
+          if (tokenMaxStdDev[token] < 2) {
+            if (tokenPrices[token] < 1.07 && tokenPrices[token] > 0.93) {
+              delete tokenEMA7Hourly[token];
+              delete tokenMaxStdDev[token];
+              sortedTokens = sortedTokens.filter(t => t !== token);
+            }
+
+          }
+        } catch (error) {
+          console.error(`Error calculating EMA and MaxStdDev for ${token}:`, error);
+          setError(error.message);
+          return null;
+        }
+
+      }
+      console.log("tokenEMA7Hourly:")
+      console.log(tokenEMA7Hourly)
+      console.log("tokenMaxStdDev:")
+      console.log(tokenMaxStdDev)
+
+      return {
+        ETH: 0.5 * amount,
+        USDC: 0.3 * amount,
+        STRK: 0.2 * amount,
+      };
+    } else if (solution === 'Offensive') {
+      return {
+        STRK: 0.7 * amount,
+        ETH: 0.2 * amount,
+        WBTC: 0.1 * amount,
+      };
+    }
+
+    return null;
+  };
   const handleInvestmentInputChange = (e) => {
     let inputValue = e.target.value;
 
@@ -149,6 +291,11 @@ function App() {
     }
 
     setInvestmentAmount(inputValue);
+  };
+
+  const handlePercentageSelect = (percentage) => {
+    const amount = (totalWalletValue * (percentage / 100)).toFixed(5);
+    setInvestmentAmount(amount);
   };
   const handleConnectWallet = async () => {
     try {
@@ -167,7 +314,12 @@ function App() {
       console.log(account);
       setAccount(account);
       setMyWalletAccount(newWalletAccount);
-      const market = await getMarket();
+      if (mode_debug === true) {
+        const market = loadTokens();
+      } else {
+        const market = await getMarket();
+      }
+
       if (market == null) {
         setError("No price feed");
       }
@@ -178,6 +330,16 @@ function App() {
       setError(err.message);
     }
   };
+
+  useEffect(() => {
+    // Update investment breakdown whenever investmentAmount changes
+    if (investmentAmount && selectedSolution) {
+      const breakdown = getInvestmentBreakdown(selectedSolution, parseFloat(investmentAmount));
+      setInvestmentBreakdown(breakdown);
+    } else {
+      setInvestmentBreakdown(null); // Clear breakdown if no amount or solution selected
+    }
+  }, [investmentAmount, selectedSolution]);
 
   useEffect(() => {
     const fetchBalances = async () => {
@@ -239,7 +401,7 @@ function App() {
         // Calculate total wallet value
         const totalWalletValue = mixedBalances.reduce((sum, item) => sum + parseFloat(item.total), 0);
         setTotalWalletValue(totalWalletValue);
-
+        setInvestmentAmount(totalWalletValue.toFixed(5));
 
       }
     };
@@ -346,7 +508,52 @@ function App() {
                 value={investmentAmount}
                 onChange={handleInvestmentInputChange} // Use the new handler
               />
+              {/* Percentage Buttons */}
+              <div>
+                <button onClick={() => handlePercentageSelect(25)}>25%</button>
+                <button onClick={() => handlePercentageSelect(50)}>50%</button>
+                <button onClick={() => handlePercentageSelect(100)}>100%</button>
+              </div>
             </div>
+          )}
+          {/* Conditionally display Investment Solution Section */}
+          {!isLoading && totalWalletValue > 0 && (
+            <>
+              <h3>2- Select investment solution</h3> {/* New section title */}
+              <div>
+                <button
+                  onClick={() => handleSolutionSelect('Secure')}
+                  style={{ backgroundColor: selectedSolution === 'Secure' ? 'lightblue' : 'white' }} >Secure</button>
+                <button
+                  onClick={() => handleSolutionSelect('Balanced')}
+                  style={{ backgroundColor: selectedSolution === 'Balanced' ? 'lightblue' : 'white' }}>Balanced</button>
+                <button
+                  onClick={() => handleSolutionSelect('Offensive')}
+                  style={{ backgroundColor: selectedSolution === 'Offensive' ? 'lightblue' : 'white' }}>Offensive</button>
+              </div>
+            </>
+          )}
+          {/* Display Investment Breakdown Table */}
+          {investmentBreakdown && (
+            <>
+              <h4>Investment Breakdown:</h4>
+              <table>
+                <thead>
+                  <tr>
+                    <th className="table-header">Token</th>
+                    <th className="table-header">Amount ($)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.entries(investmentBreakdown).map(([token, amount]) => (
+                    <tr key={token}>
+                      <td>{token}</td>
+                      <td>{amount.toFixed(2)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
           )}
 
         </>
